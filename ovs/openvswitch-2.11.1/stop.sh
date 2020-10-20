9019dce12ac07a2cf478fd8c6c3a4918c7bcbf88
gary <1286533256@qq.com>
2020-10-20 20:11:16 +0800

    s15交换机地址发生了改变

--- ovs/openvswitch-2.11.1/stop.sh
+++ ovs/openvswitch-2.11.1/stop.sh
@@ -4,6 +4,7 @@ set -x
 
 set +e
 rm -rf /var/log/gary.log
+rm -rf /var/log/kern.log
 killall ovsdb-server
 killall ovs-vswitchd
 apt-get remove openvswitch-common openvswitch-datapath-dkms openvswitch-controller openvswitch-pki openvswitch-switch
